22a02ae47a94da1a34fb586026b51f9171922730
diekrz2 <162576315+diekrz2@users.noreply.github.com>
2026-04-28 19:14:29 +0200

    daemon and usage arguments

--- src/main.c
+++ src/main.c
@@ -1,19 +1,49 @@
 #include <Elementary.h>
-#include "ui.h"
-#include "logic.h"
+#include <Ecore.h>
+#include <stdio.h>
+#include <string.h>
 #include <locale.h>
 #include <libintl.h>
 
+#include "ui.h"
+#include "logic.h"
+
 #define _(String) gettext(String)
+
+static void print_usage(void)
+{
+    printf("Usage: nl-ease [options]\n");
+    printf("  (no args)     -> start gui\n");
+    printf("  --daemon      -> start daemon mode (background)\n");
+    printf("  --help        -> show this help\n");
+}
 
 int main(int argc, char **argv)
 {
+    // Check arguments
+    if (argc > 1) {
+        if (strcmp(argv[1], "--daemon") == 0) {
+            ecore_init();
+            // no going back
+            logic_run_daemon();        
+            ecore_shutdown();         
+            return 0;
+        }
+        else if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+            print_usage();
+            return 0;
+        }
+    }
+
+    // GUI
     elm_init(argc, argv);
 
     logic_init();
+
     setlocale(LC_ALL, "");
-	bindtextdomain("nl-ease", "/usr/share/locale");
-	textdomain("nl-ease");
+    bindtextdomain("nl-ease", "/usr/share/locale");
+    textdomain("nl-ease");
+
     ui_init();
 
     elm_run();
